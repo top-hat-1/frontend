e4cf8a2806622c12b26b32f874fa88ce967c4d27
CharlyWelch <blwbiology@gmail.com>
2018-04-28 17:57:08 -0700

    add files for project-related components

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,35 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+// import { Link } from 'react-router-dom';
+import Loading from '../app/error-loading/Loading';
+import Project from './Project';
+
+class Projects extends Component {
+
+  render() {
+
+    const { projects } = this.props;
+
+    return (
+      <Fragment>
+        {projects[0] ?
+          <div>
+            <h3 className="projects-title">Projects</h3>
+            <ul className="projects-ul">
+              {projects.map((project, index) => <Project key={index} {...project} />)}
+            </ul>
+          </div>
+          : <Loading/>
+        }
+      </Fragment>
+    );
+  }
+}
+
+export default connect(
+  state => ({
+    loading: state.loading,
+    projects: state.projects
+  }),
+  null
+)(Projects);
