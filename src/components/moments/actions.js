5da8c08f886848b94b1036ca50117b4c5933fddc
CharlyWelch <blwbiology@gmail.com>
2018-04-30 17:36:02 -0700

    add actions for moments - untested/need access to api file

---
+++ src/components/moments/actions.js
@@ -0,0 +1,46 @@
+import { MOMENTS_LOAD, MOMENT_ADD, MOMENT_REMOVE, MOMENT_UPDATE } from './reducers';
+import projectsApi from '../../services/projectsApi';
+
+export function momentsLoad() {
+  return dispatch => {
+    return projectsApi.loadMoments()  //TODO: write loadMoments in projectsApi
+      .then(moments => {
+        dispatch({
+          type: MOMENTS_LOAD,
+          payload: moments
+        });
+      });
+  };
+}
+
+export function addMoment(moment) {
+  return {
+    type: MOMENT_ADD,
+    payload: projectsApi.sendMoment(moment) //TODO: write sendMoment in api
+  };
+}
+
+export function updateMoment(moment) {
+  return dispatch => {
+    return projectsApi.sendMomentUpdate(moment) // TODO: this one too
+      .then(moment => {
+        dispatch({
+          type: MOMENT_UPDATE,
+          PAYLOAD: moment
+        });
+      });
+  };
+}
+
+export function removeMoment(id) {
+  return dispatch => {
+    return projectsApi.sendRemoveMoment(id)
+      .then(() => {
+        dispatch({
+          type: MOMENT_REMOVE,
+          payload: id
+        });
+      });
+  };
+}
+
