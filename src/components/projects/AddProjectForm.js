4102b5befcbb2633c56cd864bcc118a584f6cefc
pereztjacob <pereztjacob@gmail.com>
2018-05-02 14:08:19 -0700

    remove commented out code

--- src/components/projects/AddProjectForm.js
+++ src/components/projects/AddProjectForm.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-// import { withRouter } from 'react-router-dom';
 import { addProject } from './actions';
 import AddImage from '../forms/AddImage';
 
