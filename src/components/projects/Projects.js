91753d4cb5a5e378d966944a16283c2b8e9e68de
CharlyWelch <blwbiology@gmail.com>
2018-05-09 16:58:16 -0700

    load following on app opening

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { projectLoad, projectsLoad } from './actions';
+import { getFollowing } from '../follow/actions';
 import Loading from '../app/error-loading/Loading';
 import Project from './Project';
 
@@ -11,6 +12,10 @@ class Projects extends Component {
       this.props.projectsLoad(this.props.userId) 
       :
       this.props.projectsLoad();
+    if(this.props.auth) {
+      const id = this.props.auth._id;
+      this.props.getFollowing(id);
+    }
   }
 
   render() {
@@ -37,6 +42,7 @@ export default connect(
   state => ({
     loading: state.loading,
     projects: state.projects,
+    auth: state.auth
   }),
-  { projectsLoad, projectLoad }
+  { projectsLoad, projectLoad, getFollowing }
 )(Projects);
