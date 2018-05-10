adaf9fc54cafc7906322d7323369305ad21d5ee5
CharlyWelch <blwbiology@gmail.com>
2018-05-09 17:22:01 -0700

    begin work to change follow button

--- src/components/follow/FollowButton.js
+++ src/components/follow/FollowButton.js
@@ -3,14 +3,20 @@ import { connect } from 'react-redux';
 import { addToFollowing } from './actions';
 
 class FollowButton extends PureComponent {
-
+ 
   handleClick(followId, userId){
     this.props.addToFollowing(followId, userId);
   }
 
   render() {
 
-    const { followId, userId } = this.props;
+    const { followId, userId, following } = this.props;
+    // let amFollowing = false;
+    // for(let i = 0; i < following.length; i++){
+    //   if(following[i][0].owner === userId) {
+    //     amFollowing = true;
+    //   }
+    // };   Will need to dig deeper - each position has arrays inside... 
 
     return (
       <button onClick={() => { this.handleClick(followId, userId); }}>Follow</button>
@@ -19,6 +25,8 @@ class FollowButton extends PureComponent {
 }
 
 export default connect(
-  null,
+  state => ({
+    following: state.following,
+  }),
   { addToFollowing }
 )(FollowButton);
